b38e21b1c21deb3c5b6cecfb89fbaf4d557b47b6
AhsenRiaz <64923337+AhsenRiaz@users.noreply.github.com>
2020-06-27 22:12:19 +0500

    Calculator App

---
+++ app.js
@@ -0,0 +1,20 @@
+function number(num){
+    var screen = document.getElementById("result")
+    screen.value += num; 
+    }
+    
+    function clearbutton(){
+        var screen = document.getElementById("result")
+        screen.value = "";
+    }
+    
+    function getresult(){
+        var screen = document.getElementById("result")
+        screen.value = eval(screen.value);
+    }
+    
+    function back(){
+        var screen = document.getElementById("result")
+        screen.value=screen.value.slice(0,-1)
+    }
+        
